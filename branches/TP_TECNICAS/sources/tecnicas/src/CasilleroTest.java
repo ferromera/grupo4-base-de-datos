ae9f054d0042ff84a10cd01e5f822d798cd9e788
lszperling <lszperling@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-13 16:42:36 +0000

    Cambie el nombre del metodo amenazaA de Pieza. Y un par de tests mas

--- branches/TP_TECNICAS/sources/tecnicas/src/CasilleroTest.java
+++ branches/TP_TECNICAS/sources/tecnicas/src/CasilleroTest.java
@@ -6,8 +6,8 @@ public class CasilleroTest extends TestCase {
 	
 	public void testCreacion() {
 		Casillero casillero = new Casillero(1, 2);
-		assertTrue(casillero.getJ() == 2);
-		assertTrue(casillero.getI() == 1);
+		assertTrue(casillero.getColumna() == 2);
+		assertTrue(casillero.getFila() == 1);
 		assertTrue(casillero.getPieza()==null);
 	}
 
--- branches/TP_TECNICAS/sources/tecnicas/src/Peon.java
+++ branches/TP_TECNICAS/sources/tecnicas/src/Peon.java
@@ -4,4 +4,13 @@ public class Peon extends Pieza{
 	public Peon(Casillero casillero){
 		this.inicializarPiezaCon(casillero);
 	}
+
+	public boolean amenazaA(Casillero casillero1) {
+		boolean retorno = false;
+		if ((this.casillero.getFila() == casillero1.getFila()-1)&&
+			((this.casillero.getColumna() == casillero1.getColumna()-1)||(this.casillero.getFila() == casillero1.getFila()+1))){
+			retorno = true;
+		}
+		return retorno;
+	}
 }
