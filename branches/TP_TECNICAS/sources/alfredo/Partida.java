71721dcd7ec65c216362329457911337c0777a08
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-15 02:34:03 +0000

    agrego test de enroque corto

---
+++ branches/TP_TECNICAS/sources/alfredo/Partida.java
@@ -0,0 +1,8 @@
+
+public class Partida {
+
+	public Partida(Jugador jugador1, Jugador jugador2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ branches/TP_TECNICAS/sources/alfredo/Torre.java
@@ -0,0 +1,15 @@
+
+public class Torre implements Pieza {
+
+	@Override
+	public boolean nuncaMovida() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Casillero dameCasillero() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
