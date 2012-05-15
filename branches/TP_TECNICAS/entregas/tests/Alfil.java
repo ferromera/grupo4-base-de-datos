ea7019e51a864110c58bbe250d1c80ee8d5d6b5f
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-15 19:12:27 +0000

    Arranco con Coronacion

---
+++ branches/TP_TECNICAS/entregas/tests/Alfil.java
@@ -0,0 +1,14 @@
+
+public class Alfil implements Pieza {
+
+	public Alfil(Casillero casillero) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean nuncaMovida() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
