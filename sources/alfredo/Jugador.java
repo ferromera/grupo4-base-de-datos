cf78c476e626c207e656f7b8f60d66dbe3fce350
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-15 02:34:03 +0000

    agrego test de enroque corto

---
+++ sources/alfredo/Jugador.java
@@ -0,0 +1,28 @@
+
+public class Jugador {
+
+	public Jugador(Rey rey) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pieza damePieza(Tablero.Fila uno, Tablero.Columna h) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Rey dameRey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean amenazas(Casillero casilleroF1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void enracarReyCon(Torre torreJugador1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ sources/alfredo/Tablero.java
@@ -0,0 +1,20 @@
+
+public class Tablero {
+	public enum Fila {
+		UNO, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, OCHO
+	}
+	
+	public enum Columna {
+		A, B, C, D, E, F, G, H
+	}
+
+	public Casillero dameCasillero(Fila uno, Columna d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean enJaque(Jugador jugador1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
