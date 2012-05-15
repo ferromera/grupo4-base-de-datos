6c75d110bd6f462e1d4006aa2443bfb3be0a84d8
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-15 19:40:01 +0000

    pequeño refactor de comentarios

--- entregas/tests/Peon.java
+++ entregas/tests/Peon.java
@@ -2,13 +2,16 @@
 public class Peon implements Pieza {
 
 	public Peon(Casillero casillero) {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean nuncaMovida() {
-		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public Casillero getCasillero() {
+		return null;
 	}
 
 }
