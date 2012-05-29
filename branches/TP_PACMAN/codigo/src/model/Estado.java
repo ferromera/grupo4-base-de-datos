0563a00f6d5f9446e8d8863446ed09e8da9b7546
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-29 06:09:48 +0000

    Cambio todo a state

---
+++ branches/TP_PACMAN/codigo/src/model/Estado.java
@@ -0,0 +1,16 @@
+package model;
+
+public abstract class Estado {
+	protected Fantasma fantasma;
+	protected int segundosTranscurridos; 
+	
+	protected Estado(Fantasma fantasma){
+		this.segundosTranscurridos = 0;
+		this.fantasma = fantasma;
+	}
+	
+	abstract public void esComido();
+	abstract public void pasaronSegundos(int segundos);
+	abstract public void mover();
+
+}
