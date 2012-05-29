24c0f75017e0854f6305046c6970d25bb36b3b4a
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-05-29 06:30:12 +0000

    cambio al state

--- codigo/src/model/Partida.java
+++ codigo/src/model/Partida.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Partida {
+import java.util.List;
 
+public class Partida {
+	private List<Fantasma> fantasmas;
+	
+	public Partida() {
+		this.fantasmas.add(new Fantasma());
+		this.fantasmas.add(new Fantasma());
+		this.fantasmas.add(new Fantasma());
+		this.fantasmas.add(new Fantasma());
+	}
 }
