f5c71b813184509c451c4feb29487ae2ed6cbe71
cbenez@gmail.com <cbenez@gmail.com@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-06-20 20:36:38 +0000

    ahi agregue la estrategia para que me siga compilando

--- codigo/src/model/EstrategiaPresa.java
+++ codigo/src/model/EstrategiaPresa.java
@@ -2,4 +2,22 @@ package model;
 
 public class EstrategiaPresa extends Estrategia {
 
+	@Override
+	void mover(Estado estado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void moverCazador(Estado estado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void moverPresa(Estado estado) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
