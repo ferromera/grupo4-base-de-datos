68c6d0e1bc176d669ac711bf8b2ff084007501bf
cbenez@gmail.com <cbenez@gmail.com@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-06-21 16:33:07 +0000

    cambios en la forma de la persistencia

---
+++ codigo/src/test/TestPersistencia.java
@@ -0,0 +1,29 @@
+package test;
+
+import junit.framework.TestCase;
+import commons.StoreObjectToXml;
+
+import model.Eslabon;
+import model.Estrategia;
+import model.EstrategiaZonzo;
+import model.Fantasma;
+import model.PacmanProvider;
+
+public class TestPersistencia extends TestCase{
+	public void testWriteFantasma(){
+		Estrategia estrategia = new EstrategiaZonzo(new PacmanProvider() {
+			@Override
+			public Eslabon getEslabonDePacman() {
+				return null;
+			}
+		});
+		Eslabon eslabonFantasma = new Eslabon(1, 1); 
+		Fantasma fantasma = new Fantasma(eslabonFantasma, estrategia);
+		String fileName = "PersonajesTick1.xml";
+		StoreObjectToXml storeObjectToXml = new StoreObjectToXml(fileName);
+		storeObjectToXml.writeToXml(fantasma);
+		storeObjectToXml.persistFile();
+		
+	}
+
+}
