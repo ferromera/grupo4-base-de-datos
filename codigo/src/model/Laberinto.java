c5b1fa0f14ace5b89741932bfc1206cc88feb806
pinus06 <pinus06@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-06-21 00:15:39 +0000

    agrego test para calcular si el fantasma esta en rango se realiza dibujando un triangulo espejado

--- codigo/src/model/Laberinto.java
+++ codigo/src/model/Laberinto.java
@@ -4,45 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class Laberinto {	
+public class Laberinto {
 	private final int ancho;
 	private final int alto;
-    private final Eslabon posicionInicialPacman;
-    private final Eslabon posicionSalidaFantasmas;
-    private final Map<Integer, Eslabon> eslabones;
+	private final Eslabon posicionInicialPacman;
+	private final Eslabon posicionSalidaFantasmas;
+	private final Map<Integer, Eslabon> eslabones;
 	private List<Fantasma> fantasmas;
-    private Pacman pacman;
-        
-    public Laberinto(final int ancho,final int alto, 
-    					final Eslabon posicionInicialPacman, final Eslabon posicionSalidaFantasmas,
-    					final Map<Integer, Eslabon> eslabones){
-    	this.ancho = ancho;
-    	this.alto = alto;
-    	this.posicionInicialPacman = posicionInicialPacman;
-    	this.posicionSalidaFantasmas = posicionSalidaFantasmas;
-    	this.eslabones = eslabones;
-    	
-    	this.fantasmas = new ArrayList<Fantasma>();
-    	
+	private Pacman pacman;
+
+	private static Laberinto instance = null;
+
+	public static Laberinto getInstance() {
+		return instance;
+	}
+
+	public static Laberinto valueOf(final int ancho, final int alto,
+			final Eslabon posicionInicialPacman, final Eslabon posicionSalidaFantasmas,
+			final Map<Integer, Eslabon> eslabones) {
+		instance = new Laberinto(ancho, alto, posicionInicialPacman, posicionSalidaFantasmas, eslabones);
+		return instance;
+	}
+
+	private Laberinto(final int ancho, final int alto, final Eslabon posicionInicialPacman,
+			final Eslabon posicionSalidaFantasmas, final Map<Integer, Eslabon> eslabones) {
+		this.ancho = ancho;
+		this.alto = alto;
+		this.posicionInicialPacman = posicionInicialPacman;
+		this.posicionSalidaFantasmas = posicionSalidaFantasmas;
+		this.eslabones = eslabones;
+
+		this.fantasmas = new ArrayList<Fantasma>();
+
 		this.fantasmas.add(new Fantasma(this.posicionSalidaFantasmas, new EstrategiaZonzo()));
 		this.fantasmas.add(new Fantasma(this.posicionSalidaFantasmas, new EstrategiaZonzo()));
 		this.fantasmas.add(new Fantasma(this.posicionSalidaFantasmas, new EstrategiaZonzo()));
 		this.fantasmas.add(new Fantasma(this.posicionSalidaFantasmas, new EstrategiaZonzo()));
-		
+
 		this.pacman = new Pacman(this.posicionInicialPacman);
-    }
-    
-	public void iniciaPowerPellet(){
+
+		Laberinto.instance = this;
+	}
+
+	public void iniciaPowerPellet() {
 		for (Fantasma f : fantasmas) {
 			f.iniciaPowerPellet();
 		}
 	}
-	
-	public Eslabon getEslabonDePacman(){
-		return this.pacman.getEslabon();	
+
+	public Eslabon getEslabonDePacman() {
+		return this.pacman.getEslabon();
 	}
-	public Pacman getPacman(){
+
+	public Pacman getPacman() {
 		return pacman;
 	}
-	
+
 }
--- codigo/src/test/testLaberinto.java
+++ codigo/src/test/testLaberinto.java
@@ -3,26 +3,31 @@ package test;
 import java.util.HashMap;
 import java.util.Map;
 
-import parsers.LaberintoParser;
 import junit.framework.TestCase;
 import model.Eslabon;
 import model.Laberinto;
+import parsers.LaberintoParser;
 
 public class testLaberinto extends TestCase {
 	private Laberinto laberinto;
 	
-	public void testLoadFromXml(){
+	@Override
+	public void setUp(){
 		laberinto = LaberintoParser.build("laberinto.xml");		
 	}
+	
 	public void testCrearLaberinto() {
 		Eslabon posicionInicialPacman = new Eslabon(1, 2);
 		Eslabon posicionSalidaFantasmas = new Eslabon(4, 4);
 		Map<Integer, Eslabon> eslabones = new HashMap<Integer, Eslabon>();
-		laberinto = new Laberinto(10, 10, posicionInicialPacman , posicionSalidaFantasmas , eslabones );
+		laberinto = Laberinto.valueOf(10, 10, posicionInicialPacman , posicionSalidaFantasmas , eslabones );
 		assertEquals(laberinto.getEslabonDePacman(),  posicionInicialPacman);
 		assertEquals(laberinto.getPacman().getEslabon(), posicionInicialPacman);
 	}
 	
-	
+	public void testGetInstance(){
+		assertNotNull(Laberinto.getInstance());
+		assertEquals(Laberinto.getInstance().getEslabonDePacman(), laberinto.getEslabonDePacman());
+	}
 	
 }
