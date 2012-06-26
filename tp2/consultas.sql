4e78a4702b930067b7911a44e1db95054192b516
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@7ae171d6-d976-3702-c5b4-7566f7369f44>
2012-06-26 04:45:03 +0000

    Las 10 consultas funcionando ok para los datos

--- tp2/consultas.sql
+++ tp2/consultas.sql
@@ -21,18 +21,13 @@ select r1.tipodoc,r1.numerodoc,r1.nombre from paciente r1,afiliado a
 
 /* 3-
 */
-select r2.numerodoc from afiliado r2
-where exists (
-	select max(numeroafiliado),obrasocial from afiliado r1
-	group by r1.obrasocial
-	having r1.obrasocial = r2.obrasocial and r2.numeroafiliado = max(numeroafiliado)
+select obraSocial,p.* from afiliado a,paciente p
+where p.tipoDoc=a.tipoDoc and p.numeroDoc=a.numeroDoc
+    and exists (
+	select max(numeroafiliado),obrasocial from afiliado a2
+	group by a2.obrasocial
+	having a2.obrasocial = a.obrasocial and a.numeroafiliado = max(numeroafiliado)
 	);
-
-select * from paciente p where exists(
-	select numerodoc from afiliado a where numerodoc not in (
-		select r2.numerodoc from afiliado r2, afiliado r1
-		where r2.numeroafiliado < r1.numeroafiliado and r1.obrasocial = r2.obrasocial
-	) and a.tipodoc = p.tipodoc and a.numerodoc = p.numerodoc);
 
 /*
 4. Matriculas de los profesionales que tienen indicaciones medicas para
@@ -52,12 +47,13 @@ select distinct(r1.matricula) from indicacionmedica r1 where not exists (
 5. Matriculas de los profesionales que no tienen indicaciones medicas para
 Arnaldo Andre.
 */
-select r1.matricula from indicacionmedica r1 where not exists (
-			select * from paciente r2
-			where r2.tipoDoc = r1.tipoDoc
-      and r2.numeroDoc = r1.numeroDoc
-      and r2.nombre = 'Arnaldo'
-      and r2.apellido = 'Andre'
+select distinct matricula from indicacionmedica i1 
+where matricula not in(
+	select matricula from paciente p,indicacionmedica i2
+		where i2.tipoDoc = p.tipoDoc
+          and i2.numeroDoc = p.numeroDoc
+          and p.nombre = 'Arnaldo'
+          and p.apellido = 'Andre'
 );
 
 /*
@@ -75,8 +71,21 @@ group by obraSocial, plan);
 7. Para todos los pacientes con al menos una indicacion medica, el nombre y plan de su
  cobertura. En caso de no tener, indicar 'SIN COBERTURA' en ambos campos(Hipotesis tomada:
  1 paciente puede estar afiliado a 1 plan de una obra social o a ninguno).
+*/
+select nombre,apellido,obraSocial,plan
+from paciente p, afiliado a
+where  a.numeroDoc = p.numeroDoc
+   and   a.tipoDoc = p.tipoDoc
+   and   exists ( select * from indicacionmedica i
+                       where i.tipoDoc = p.tipoDoc and i.numeroDoc = p.numeroDoc )
+union
+select  nombre,apellido, 'SIN COBERTURA', 'SIN COBERTURA'
+from  paciente p
+where   exists ( select * from indicacionmedica i
+                       where i.tipoDoc = p.tipoDoc and i.numeroDoc = p.numeroDoc )
+   and   not exists ( select * from afiliado a
+                             where p.tipoDoc = a.tipoDoc and p.numeroDoc = a.numeroDoc );
 /*
-
 8. Para cada obra social, su promedio de cantidad de pacientes por plan.
 */
 select obraSocial, count(*) /count(distinct(plan)) 'promedio'
@@ -88,15 +97,16 @@ select obraSocial, count(*) /count(distinct(plan)) 'promedio'
 */
 
 select NumeroQuirofano
-    from IndicacionMedica
+    from indicacionmedica
         group by NumeroQuirofano
             having count(*)>= all(Select count(*)
-                from IndicacionMedica
+                from indicacionmedica
                     group by NumeroQuirofano);
 
 /* 
 10. Para todos los pacientes del sistema, la cantidad de indicaciones medicas que tiene
- (indicar 0 en caso de no tener).
+ (indicar 0 en caso 
+ de no tener).
 */
 
 select tipoDoc , numeroDoc, count(*) from indicacionmedica
